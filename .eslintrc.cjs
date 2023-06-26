a3543272cddd0d3afee760a6573686f79bf90e5d
Johan <johan.9te@gmail.com>
2023-06-26 17:28:02 +0200

    First init commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,72 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true,
+		"node": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react/recommended"
+	],
+	"overrides": [
+		{
+			"env": {
+				"node": true
+			},
+			"files": [
+				".eslintrc.{js,cjs}"
+			],
+			"parserOptions": {
+				"sourceType": "script"
+			}
+		}
+	],
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"plugins": [
+		"@typescript-eslint",
+		"react"
+	],
+	"rules": {
+		"indent": [
+			"error", "tab"
+		],
+		"linebreak-style": [
+			"error", "windows"
+		],
+		"quotes": [
+			"error", "double"
+		],
+		"semi": [
+			"error", "always"
+		],
+		"eol-last": [
+			"error", "always"
+		],
+		"no-mixed-spaces-and-tabs": [
+			"error", "smart-tabs"
+		],
+		"no-trailing-spaces": "error",
+		"no-whitespace-before-property": "error",
+		"operator-linebreak": [
+			"error", "before"
+		],
+		"comma-dangle": [
+			"error", "never"
+		],
+		"array-bracket-newline": [
+			"error", {
+				"multiline": true,
+				"minItems": 1
+			}
+		],
+		"object-property-newline": "error",
+		"object-curly-spacing": [
+			"error", "always"
+		]
+	}
+};
